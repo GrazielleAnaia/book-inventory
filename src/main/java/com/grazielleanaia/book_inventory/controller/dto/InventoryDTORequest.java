21849d5085f3f245f51d6231d8f43278c36b5f3a
Grazielle <grazielle.anaia@gmail.com>
2025-08-28 11:22:07 -0400

    improvements

--- src/main/java/com/grazielleanaia/book_inventory/controller/dto/InventoryDTORequest.java
+++ src/main/java/com/grazielleanaia/book_inventory/controller/dto/InventoryDTORequest.java
@@ -1,12 +1,13 @@
 package com.grazielleanaia.book_inventory.controller.dto;
 
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
-public record InventoryDTORequest(String title,
-                                  String author,
-                                  String isbn,
-                                  Integer numberCopies,
-                                  LocalDate publicationDate) {
+public record InventoryDTORequest(@NotBlank(message = "title is required") String title,
+                                  @NotBlank(message = "Author is required") String author,
+                                  @NotBlank(message = "isbn is required") String isbn,
+                                  @NotNull(message = "number of copies is required") Integer numberCopies,
+                                  @NotNull(message = "publication date is required") LocalDate publicationDate) {
 }
